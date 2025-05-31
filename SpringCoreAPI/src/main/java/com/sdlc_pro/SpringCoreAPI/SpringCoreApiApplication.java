ef6185d1eee212c9a6490ae282b9ef16d837353c
ZAHIDUL SIKDER <120940107+zahidulsikder@users.noreply.github.com>
2025-05-31 14:53:57 +0600

    Created Project

---
+++ SpringCoreAPI/src/main/java/com/sdlc_pro/SpringCoreAPI/SpringCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.sdlc_pro.SpringCoreAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreApiApplication.class, args);
+	}
+
+}
---
+++ SpringCoreAPI/src/test/java/com/sdlc_pro/SpringCoreAPI/SpringCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdlc_pro.SpringCoreAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
